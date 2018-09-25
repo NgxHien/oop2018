af5e50f59bc253f51eee66626c092d5a1158ee49
NgxHien <nguyenxuanhientb@icloud.com>
2018-09-26 00:38:31 +0700

    Update 26/9/2018

--- src/week2/task3/Task3.java
+++ src/week2/task3/Task3.java
@@ -4,3 +4,89 @@ public class Task3 {
 }
 
 //TODO: khai báo 3 class tương ứng với 3 đối tượng thực tế ở dưới
+
+ class Car{
+    String  Type,
+            Name,
+            Color,
+            Price;
+    public void setName(String name){
+        this.Name = name;
+    }
+    public String getName(){
+        return this.Name;
+    }
+
+    public void setType(String type){
+        this.Type = type;
+    }
+    public String getType(){
+        return this.Type;
+    }
+
+    public void setColor(String color){
+        this.Color = color;
+    }
+    public String getColor(){
+        return this.Color;
+    }
+
+    public void setPrice(String price){
+        this.Price = price;
+    }
+    public String getPrice(){
+        return this.Price;
+    }
+}
+
+ class Dog{
+    String  Name,
+            CoatColor,
+            Species;
+    public void setName(String name){
+        this.Name = name;
+    }
+    public String getName(){
+        return this.Name;
+    }
+
+    public void setCoatColor(String coatColor){
+        this.CoatColor = coatColor;
+    }
+    public String getCoatColor(){
+        return this.CoatColor;
+    }
+
+    public void setSpecies(String species){
+        this.Species = species;
+    }
+    public String getSpecies(){
+        return this.Species;
+    }
+}
+
+class Cat{
+    String  Name,
+            CoatColor,
+            Species;
+    public void setName(String name){
+        this.Name = name;
+    }
+    public String getName(){
+        return this.Name;
+    }
+
+    public void setCoatColor(String coatColor){
+        this.CoatColor = coatColor;
+    }
+    public String getCoatColor(){
+        return this.CoatColor;
+    }
+
+    public void setSpecies(String species){
+        this.Species = species;
+    }
+    public String getSpecies(){
+        return this.Species;
+    }
+}
