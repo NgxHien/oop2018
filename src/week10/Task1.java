dd09a8eab4a95888a0ae80fbfb204552955eb5f6
NgxHien <nguyenxuanhientb@icloud.com>
2018-11-14 14:02:14 +0700

    ...

--- src/week10/Task1.java
+++ src/week10/Task1.java
@@ -1,4 +1,15 @@
 package week10;
 
+import week9.Utils;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Task1 {
+    public List<String> getAllFunctions(File path){ // path = "src\\week9\\Utils.java"
+        List<String> list = new ArrayList<>();
+
+        return null;
+    }
 }
--- src/week10/Task2.java
+++ src/week10/Task2.java
@@ -1,4 +1,28 @@
 package week10;
 
+import java.util.Random;
+
 public class Task2 {
+    static void bubbleSort(double arr[])
+    {
+        double n = arr.length;
+        for (int i = 0; i < n-1; i++)
+            for (int j = 0; j < n-i-1; j++)
+                if (arr[j] > arr[j+1])
+                {
+                    // swap temp and arr[i]
+                    double temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+                }
+        for (int i=0; i<n; ++i)
+            System.out.println(arr[i]);
+    }
+    public static void main(String[] args){
+        Random rd = new Random();
+        double[] a = new double[1000];
+        for (int i = 0 ;i<1000 ;i++)
+            a[i] =(rd.nextDouble())*235;
+        bubbleSort(a);
+    }
 }
