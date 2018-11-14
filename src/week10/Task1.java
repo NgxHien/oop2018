2092fd20e6d8082566efc30438ec8f6fc85585a3
NgxHien <nguyenxuanhientb@icloud.com>
2018-11-14 14:47:52 +0700

    ...

--- src/week10/Task1.java
+++ src/week10/Task1.java
@@ -1,15 +1,41 @@
 package week10;
 
-import week9.Utils;
-
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class Task1 {
-    public List<String> getAllFunctions(File path){ // path = "src\\week9\\Utils.java"
+    List<String> getAllFunctions(File path){ // path = "src\\week9\\Utils.java"
         List<String> list = new ArrayList<>();
+        try {
+            Scanner sc = new Scanner(path);
+            while(sc.hasNext()){
+                String func = sc.nextLine();
+                if(func.contains("static")) {
+                    while (!sc.hasNext("}"))
+                        func = func + "\n" + sc.nextLine();
+                    func = func + "\n}";
+                    list.add(func);
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return list;
+    }
 
-        return null;
+    public String findFunctionByName(String name){
+        String justname = name.substring(0, name.indexOf("(") -1);
+        List<String> allFunction = getAllFunctions(new File("src\\week9\\Utils.java"));
+        for (int i=0;i < allFunction.size(); i++)
+            if (allFunction.get(i).contains(justname))
+                return allFunction.get(i);
+        return "Function not found";
+    }
+    public static void main(String[] args){
+        Task1 test = new Task1();
+        List<String> list = test.getAllFunctions(new File("src\\week9\\Utils.java"));
     }
 }
