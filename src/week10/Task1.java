a7c157c63503dcfceef5a8a99b1697c7338855b7
NgxHien <nguyenxuanhientb@icloud.com>
2018-11-14 14:50:57 +0700

    ...

--- src/week10/Task1.java
+++ src/week10/Task1.java
@@ -6,6 +6,14 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class Task1 {
     List<String> getAllFunctions(File path){ // path = "src\\week9\\Utils.java"
         List<String> list = new ArrayList<>();
@@ -16,7 +24,7 @@ public class Task1 {
                 if(func.contains("static")) {
                     while (!sc.hasNext("}"))
                         func = func + "\n" + sc.nextLine();
-                    func = func + "\n}";
+                    func = func + "\n\t}";
                     list.add(func);
                 }
             }
@@ -34,8 +42,11 @@ public class Task1 {
                 return allFunction.get(i);
         return "Function not found";
     }
-    public static void main(String[] args){
-        Task1 test = new Task1();
-        List<String> list = test.getAllFunctions(new File("src\\week9\\Utils.java"));
+    public static void main(String[] args) {
+
+        List<String> list = new Task1().getAllFunctions(new File("src/week9/Utils.java"));
+        File file = new File("output.txt");
+        System.out.println(new Task1().findFunctionByName("main(String[])"));
+
     }
 }
